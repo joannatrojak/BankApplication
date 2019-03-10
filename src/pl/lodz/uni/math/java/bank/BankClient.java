75bf0ba48d8f0c2e75766893fa06ebe01ad63c9d
Joanna <joasia405@gmail.com>
2019-03-10 11:22:12 +0100

    test dateOfJoininig

--- src/pl/lodz/uni/math/java/bank/BankClient.java
+++ src/pl/lodz/uni/math/java/bank/BankClient.java
@@ -1,12 +1,22 @@
 package pl.lodz.uni.math.java.bank;
 
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class BankClient {
+
+public final class BankClient {
     private String firstName = null;
     private String lastName;
-    private String dateOfBirth;
-    private String dateOfJoining;
+    private Date dateOfBirth;
+    private final String dateOfJoining;
+
+    public BankClient() throws ParseException {
+        Date date = new Date();
+        this.dateOfJoining = new SimpleDateFormat("dd/MM/yyyy").format(date);
+    }
 
     public String getFirstName() {
         return firstName;
@@ -24,11 +34,21 @@ public class BankClient {
         this.lastName = lastName;
     }
 
-    public String getDateOfBirth() {
-        return dateOfBirth;
+    public String getDateOfBirth() throws Exception {
+        try{
+            String pattern = "dd/MM/yyyy";
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            return simpleDateFormat.format(dateOfBirth);
+        } catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+
+    }
+    public void setDateOfBirth(String dateOfBirth) throws ParseException {
+        this.dateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse(dateOfBirth);
     }
 
-    public void setDateOfBirth(String dateOfBirth) {
-        
+    public String getDateOfJoining() {
+        return dateOfJoining;
     }
 }
--- src/pl/lodz/uni/math/java/tests/BankClientTest.java
+++ src/pl/lodz/uni/math/java/tests/BankClientTest.java
@@ -2,40 +2,49 @@ package pl.lodz.uni.math.java.tests;
 
 import org.junit.jupiter.api.Test;
 import pl.lodz.uni.math.java.bank.BankClient;
+
+import java.text.ParseException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankClientTest {
     @Test
-    public void hasFirstName(){
+    public void hasFirstName() throws ParseException {
         BankClient tester = new BankClient();
         assertEquals(null, tester.getFirstName());
     }
     @Test
-    public void setFirstName(){
+    public void setFirstName() throws ParseException {
         BankClient tester = new BankClient();
         tester.setFirstName("John");
         assertEquals("John", tester.getFirstName());
     }
     @Test
-    public void hasLastName(){
+    public void hasLastName() throws ParseException {
         BankClient tester = new BankClient();
         assertEquals(null, tester.getLastName());
     }
     @Test
-    public void setLastName(){
+    public void setLastName() throws ParseException {
         BankClient tester = new BankClient();
         tester.setLastName("Smith");
         assertEquals("Smith", tester.getLastName());
     }
     @Test
-    public void hasDateOfBirth(){
+    public void hasDateOfBirth() throws Exception {
         BankClient tester = new BankClient();
         assertEquals(null, tester.getDateOfBirth());
     }
     @Test
-    public void setDateOfBirth(){
+    public void setDateOfBirth() throws Exception {
         BankClient tester = new BankClient();
-        tester.setDateOfBirth("19-01-1993");
-        assertEquals("19-01-1993", tester.getDateOfBirth());
+        tester.setDateOfBirth("19/01/1993");
+        assertEquals("19/01/1993", tester.getDateOfBirth());
     }
+    @Test
+    public void hasDateOfJoining() throws ParseException {
+        BankClient tester = new BankClient();
+        assertEquals("10/03/2019", tester.getDateOfJoining());
+    }
+
 }
